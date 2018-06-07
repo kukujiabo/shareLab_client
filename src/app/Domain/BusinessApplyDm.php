1802cb34d9e91c333ef9d12b927e126ca051997d
kukujiabo <kukujiabo@163.com>
2018-06-08 07:49:49 +0800

    接口添加参数

--- src/app/Domain/BusinessApplyDm.php
+++ src/app/Domain/BusinessApplyDm.php
@@ -3,7 +3,21 @@ namespace App\Domain;
 
 class BusinessApplyDm {
 
+  protected $_member; 
+
+  public function __construct() {
+  
+    $requestHeader = getallheaders();
+  
+    $auth = RedisClient::get('member_auth', $requestHeader['CX-TOKEN']);
+
+    $this->_member = $auth;
+
+  }
+
   public function create($params) {
+
+    $params['member_id'] = $this->_member->id;
   
     return \App\request('App.BusinessApply.Create', $params);
   
