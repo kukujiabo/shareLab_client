83160bc2648db9a511c8dcfb99747e98baa1fe59
kukujiabo <kukujiabo@163.com>
2018-06-08 09:14:15 +0800

    修改接口参数

--- src/app/Domain/RewardDm.php
+++ src/app/Domain/RewardDm.php
@@ -3,7 +3,21 @@ namespace App\Domain;
 
 class RewardDm {
 
+  protected $_member; 
+
+  public function __construct() {
+  
+    $requestHeader = getallheaders();
+  
+    $auth = RedisClient::get('member_auth', $requestHeader['CX-TOKEN']);
+
+    $this->_member = $auth;
+
+  }
+
   public function rewardShopUnionList($data) {
+
+    $data['member_id'] = $this->_member->id;
   
     return \App\request('App.Reward.RewardShopUnionList', $data);
   
